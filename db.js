f4fa8b28042a139a5a13d0e6e0567139bedd970b
arafatul-islam <marafatbs@gmail.com>
2022-10-18 23:20:47 +0600

    hotel crud and error handler created

--- db.js
+++ db.js
@@ -3,18 +3,17 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     await mongoose.connect(process.env.DB);
-    console.log("mongodb connected!");
   } catch (error) {
     throw error;
   }
 };
 
-mongoose.connection.on("disconnected",()=>{
-    console.log("mongodb disconnected!");
+mongoose.connection.on("disconnected", () => {
+  console.log("mongodb disconnected!");
 });
 
-mongoose.connection.on("connected",()=>{
-    console.log("mongodb connected!");
-})
+mongoose.connection.on("connected", () => {
+  console.log("mongodb connected!");
+});
 
-export default connect
+export default connect;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose, { connect } from "mongoose";
 
-//
+//own
 import database from "./db.js";
+import authRouter from "./routes/authRoute.js";
+import userRouter from "./routes/userRoute.js";
+import hotelRouter from "./routes/hotelRoute.js";
+import roomRouter from "./routes/roomRoute.js";
 
 const app = express();
 dotenv.config();
@@ -11,8 +14,26 @@ database();
 const port = process.env.PORT || 8800;
 
 // middleware
-app.get("/", (req, res) => {
-  res.send("sending request!");
+app.use(express.json());
+
+app.use("/api/register", authRouter);
+app.use("/api/login", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+
+app.use((err, req, res, next) => {
+  const errorsStatus = err.status || 500;
+  const errorsCode = err.code || 404;
+  const errorsMessage = err.message || "something went wrong!!";
+
+  return res.status(errorsStatus).json({
+    success: false,
+    status: errorsStatus,
+    code: errorsCode,
+    message: errorsMessage,
+    stack: err.stack,
+  });
 });
 
 app.listen(port, () => console.log("server running on port", port));
